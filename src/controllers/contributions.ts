c4a7cfd5f2339b94b2b9d173e123e9a02d9f43d4
Thanh Vu <csnsjh@gmail.com>
2021-09-24 04:30:53 +0700

    fix

--- src/controllers/contributions.ts
+++ src/controllers/contributions.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { GetContributionsDto } from '../dtos/getContributionsDto';
+import { GetContributionsDto } from '../dtos/GetContributionsDto';
 import { GetYearsDto } from '../dtos/GetYearsDto';
 import * as contributionsService from '../services/contributions';
 
--- src/controllers/pinnedProjects.ts
+++ src/controllers/pinnedProjects.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { GetContributionsDto } from '../dtos/getContributionsDto';
+import { GetContributionsDto } from '../dtos/GetContributionsDto';
 import * as pinnedProjectsService from '../services/pinnedProjects';
 
 export const getPinnedProjects = async (req: Request, res: Response) => {
