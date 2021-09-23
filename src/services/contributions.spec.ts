d98fa4ce1bc309a91bba604d987310204b7569f8
Thanh Vu <csnsjh@gmail.com>
2021-09-24 03:33:00 +0700

    fix

--- src/services/contributions.spec.ts
+++ src/services/contributions.spec.ts
@@ -6,7 +6,7 @@ describe('Gets years', () => {
   it('Returns a array of years', async () => {
     const years = await contributionsService.getYears(username);
     const url = years[0].url;
-    const value = years[0].value;
+    const value = years[0].year;
     expect(years.length).toBeGreaterThan(0);
     expect(url).not.toEqual('');
     expect(url).not.toBeNull();
@@ -20,7 +20,7 @@ describe('Gets years', () => {
 describe('Gets data for year by url', () => {
   it('Returns data for one year', async () => {
     const data = await contributionsService.getDataForYearByUrl({
-      value: '2020',
+      year: 2020,
       url: '/misa198?tab=overview&from=2020-12-01&to=2020-12-31',
     });
     const day = data[0];
